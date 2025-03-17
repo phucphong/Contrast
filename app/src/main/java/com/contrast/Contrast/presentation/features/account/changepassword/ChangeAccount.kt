065ff6381a41b9b29c40aa9b04e9bf34618750c7
phucphong <phucphonghd@gmail.com>
2025-03-17 16:13:44 +0700

    no message

--- app/src/main/java/com/contrast/Contrast/presentation/features/account/changepassword/ChangeAccount.kt
+++ app/src/main/java/com/contrast/Contrast/presentation/features/account/changepassword/ChangeAccount.kt
@@ -2,8 +2,7 @@ package com.contrast.Contrast.presentation.features.account.changepassword
 
 class ChangeAccount {
     val  a :String =""
-    val  b :String =""
-    val  c :String =""
+   
     val  y :String =""
     val  U :String =""
 }
